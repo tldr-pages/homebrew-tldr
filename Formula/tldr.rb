43432079613affe1eda7b7c50d9fba3f139ae763
Leandros Linux <dev@arvid-g.de>
2014-05-13 14:23:48 +0200

    update formula to 0.2

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Tldr < Formula
   homepage "https://github.com/tldr-pages/tldr"
-  url "https://github.com/tldr-pages/tldr-cpp-client/archive/0.1.tar.gz"
-  sha1 "18c133b70c9818f04fa9ba449f48fce6e4ee14b4"
+  url "https://github.com/tldr-pages/tldr-cpp-client/archive/0.2.tar.gz"
+  sha1 "43c16961a87dc6e42a6227196c757e9980a4445e"
 
   depends_on "curl"
   def install
