3f639298b77cbc64f5df996b2827c31f9fe54c94
Arvid Gerstmann <dev@arvid-g.de>
2016-01-17 23:58:25 +0100

    add head to formula

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -2,8 +2,11 @@ require "formula"
 
 class Tldr < Formula
   homepage "https://github.com/tldr-pages/tldr"
+
   url "https://github.com/tldr-pages/tldr-cpp-client/archive/v1.1.0.tar.gz"
   sha1 "7323a59434d5c3f910e183e77b62c941b2834869"
+
+  head "https://github.com/tldr-pages/tldr-cpp-client.git"
 
   depends_on "curl"
   depends_on "libzip"
